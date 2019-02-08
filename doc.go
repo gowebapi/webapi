0484fbe5f0a823224865353e227261e4d6641b03
Martin Juhlin <martin.juhlin@gmail.com>
2019-02-08 12:56:28 +0100

    change package structure

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Go Language Web Assembly bindings for DOM, HTML etc
+//
+// > __WARNING__: The current API is in very early state and should be consider to be expremental. There is upcommig changed like moving types into multiple packages.
+//
+// This library is trying to be feature complete and up to date with current standard to let everything a browser having to offect be available in WebAssembly in a Go API accessable. To achive this most
+// of the code is [generated](https://github.com/gowebapi/webidlgenerator) from [WebIDL files](https://github.com/gowebapi/idl). WebIDL files can be found in different standard making it easiser to be up to date than handwritten binding code.
+package webapi
