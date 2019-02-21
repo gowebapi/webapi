eb722ac2cca8969737a12ef34d2ad0cb139ffca6
Martin Juhlin <martin.juhlin@gmail.com>
2019-02-21 23:04:56 +0100

    more package doc updates

--- doc.go
+++ doc.go
@@ -2,12 +2,10 @@
 //
 // WARNING: The current API is in very early state and should be consider
 // to be expremental. There is upcommig changed like moving types into
-// multiple packages.
+// multiple packages will occur when more of the browser standard
+// is added.
 //
-// This library is trying to be feature complete and up to date with current
-// standard to let everything a browser having to offect be available in
-// WebAssembly in a Go API accessable. To achive this most of the code is
-// generated with https://github.com/gowebapi/webidlgenerator.
-// Input is WebIDL files that contains specification on browser javascript API.
+// Please note that there is server subdirectory below this package, see below.
 //
+// See home page https://gowebapi.github.io for more details.
 package webapi
