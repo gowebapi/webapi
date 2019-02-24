d94e34649cedc00cb0eea9b91f9f17efa7f100f9
Martin Juhlin <martin.juhlin@gmail.com>
2019-02-24 17:40:04 +0100

    document fix

--- payment/request/doc.go
+++ payment/request/doc.go
@@ -1,2 +1,2 @@
-// Package request allow merchants (i.e. web sites selling physical or digital goods) to utilize one or more payment methods with minimal integration.
+// Package request allow merchants (like web sites selling physical or digital goods) to utilize one or more payment methods with minimal integration.
 package request
