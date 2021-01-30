f030745be09498e3a7889d592595a2ae2a32f04c
Jan Pfeifer <pfeifer@gmail.com>
2021-01-30 09:27:46 +0100

    Small doc improvement.

--- core/jsarray/doc.go
+++ core/jsarray/doc.go
@@ -1,3 +1,4 @@
-// Pacakge jsarray is converting go primitive arrays into
-// javascript Array types (not TypedArray!)
+// Package jsarray provides functions to convert go slices into/from javascript Array types.
+// This is not for TypedArray's, see package jsconv for that -- TypedArrays are an order of
+// magnitude faster to convert to/from js.
 package jsarray
